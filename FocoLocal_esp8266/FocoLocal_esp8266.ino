1cd092c5a57026265b49a78cc11409459bb39e47
chepecarlos <chepecarlos@alswblog.org>
2018-10-16 21:23:45 -0600

    Codigo mejorando con mqtt

--- FocoLocal_esp8266/FocoLocal_esp8266.ino
+++ FocoLocal_esp8266/FocoLocal_esp8266.ino
@@ -7,6 +7,8 @@ const char* mqtt_server = "broker.mqtt-dashboard.com";
 
 int Foco[] = {0, 4, 16};
 int Boton[] = {2, 14, 12};
+boolean EstadoFoco[] = {false, false, false};
+boolean EstadoAnterior[] = {true, true, true};
 int LedIndicador = 5;
 
 WiFiClient espClient;
@@ -52,23 +54,32 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+  Serial.print("Mensaje de[");
   Serial.print(topic);
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  
+  if ((char)payload[0] == 'a' || (char)payload[0] == 'A') {
+    EstadoFoco[0] = true;
   }
-
+  else if ((char)payload[0] == 'b' || (char)payload[0] == 'B') {
+    EstadoFoco[0] = false;
+  }
+  else if ((char)payload[0] == 'c' || (char)payload[0] == 'C') {
+    EstadoFoco[1] = true;
+  }
+  else if ((char)payload[0] == 'd' || (char)payload[0] == 'D') {
+    EstadoFoco[1] = false;
+  }
+  else if ((char)payload[0] == 'e' || (char)payload[0] == 'E') {
+    EstadoFoco[2] = true;
+  }
+  else if ((char)payload[0] == 'f' || (char)payload[0] == 'F') {
+    EstadoFoco[2] = false;
+  }
 }
 
 void reconnect() {
@@ -76,10 +87,10 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266ClientALSW")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("ALSWout", "hello world");
+      //client.publish("ALSWout", "hello world");
       // ... and resubscribe
       client.subscribe("ALSWin");
     } else {
@@ -93,35 +104,52 @@ void reconnect() {
 }
 void loop() {
   ActualizarBotones();
+  ActualizarFoco();
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  long now = millis();
+ long now = millis();
   if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-    snprintf (msg, 75, "hello world #%ld", value);
+    snprintf (msg, 75, "Foco ALSW F0-%d F1-%d, F2-%d",EstadoFoco[0],EstadoFoco[1],EstadoFoco[2]);
     Serial.print("Publish message: ");
     Serial.println(msg);
     client.publish("ALSWout", msg);
   }
+
   digitalWrite(LedIndicador, 1);
   delay(250);
   digitalWrite(LedIndicador, 0);
   delay(250);
 }
 
+void ActualizarFoco() {
+  for (int i = 0; i < 3; i++) {
+    if ( EstadoFoco[i] != EstadoAnterior[i]) {
+      EstadoAnterior[i] = EstadoFoco[i];
+      if (EstadoFoco[i]) {
+        digitalWrite(Foco[i] , 1);
+        Serial.print("Encender " );
+      }
+      else {
+        digitalWrite(Foco[i] , 0);
+        Serial.print("Apagar " );
+      }
+      Serial.println(i);
+    }
+  }
+}
+
 void ActualizarBotones() {
   for (int i = 0; i < 3; i++) {
     if (digitalRead(Boton[i]) == 0) {
-      Serial.print("A");
-    }
-    else {
-      Serial.print("B");
+      EstadoFoco[i] = !EstadoFoco[i];
+      delay(50);
+      while (digitalRead(Boton[i]) == 0) {
+      }
     }
   }
-  Serial.println();
-
 }
