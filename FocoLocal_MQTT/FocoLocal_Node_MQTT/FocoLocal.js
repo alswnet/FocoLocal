acb27b40c1b8aaf30419ae527f5ce277e747a04e
chepecarlos <chepecarlos@alswblog.org>
2019-03-20 20:48:08 -0600

    Mejorar en menu y estado

--- FocoLocal_MQTT/FocoLocal_Node_MQTT/FocoLocal.js
+++ FocoLocal_MQTT/FocoLocal_Node_MQTT/FocoLocal.js
@@ -1,3 +1,5 @@
+// TODO: Ver pro que se apaga todo por error al apagar y encender rapido
+
 const TelegramBot = require('node-telegram-bot-api');
 var config = require('./config.js');
 var mqtt = require('mqtt');
@@ -5,10 +7,8 @@ const bot = new TelegramBot(config.token, {
   polling: true
 });
 
-// TODO: Sistem de cambio con 1 digito
-// TODO: Mayuscula y miniscula
-// TODO: Estado sistema
-// TODO: Ver pro que se apaga todo por error al apagar y encender rapido
+var Estado = [0, 0, 0, 0, 0];
+var CantidadFoco = 3;
 var client = mqtt.connect('mqtt://broker.mqtt-dashboard.com');
 
 client.on('connect', function() {
@@ -16,52 +16,70 @@ client.on('connect', function() {
   client.subscribe('ALSW/foco2estado');
   client.subscribe('ALSW/foco3estado');
   client.subscribe('ALSW/foco4estado');
-
-//  client.subscribe('ALSW/foco1');
-//  client.subscribe('ALSW/foco2');
-//  client.subscribe('ALSW/foco3');
-//  client.subscribe('ALSW/foco4');
 })
 
 client.on('message', function(topic, message) {
-  console.log("Topic " + topic + " - " + message.toString());
-  if(topic.indexOf("ALSW/foco") == 0){
-    
+  var IDTopic = topic.substring(9, 10);
+  IDTopic = Number(IDTopic);
+  if (message.toString() == "1") {
+    Estado[IDTopic] = "Encendido"
+  } else {
+    Estado[IDTopic] = "Apagado"
   }
+  console.log("Foco " + IDTopic + " Estado " + Estado[IDTopic]);
 })
-
-
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   var Mensaje = msg.text;
-  var MensajeMinus = Mensaje.toLowerCase();
-  if (MensajeMinus == "estado" || MensajeMinus == "e") {
-    bot.sendMessage(chatId, 'No se XD');
-  } else if (MensajeMinus == "apagartodo" || MensajeMinus == "at") {
+  Mensaje = Mensaje.toLowerCase();
+  if (Mensaje == "ayuda" || Mensaje == "inicio" || Mensaje == "\/start") {
+    bot.sendMessage(chatId, "Foco Inteligente ChatBot de Local ALSW comandos:")
+      .then(() => {
+        return bot.sendMessage(chatId, "Encender o \"E\" mas numero ej: E1")
+      })
+      .then(() => {
+        return bot.sendMessage(chatId, "Apagar o \"A\" mas numero ej: Apagar 2")
+      })
+      .then(() => {
+        return bot.sendMessage(chatId, "EnenderTodo o \"ET\"")
+      })
+      .then(() => {
+        return bot.sendMessage(chatId, "ApagarTodo o \"AT\"")
+      })
+      .then(() => {
+        return bot.sendMessage(chatId, "Estado o \"E\"")
+      })
+      .then(() => {
+        return bot.sendMessage(chatId, "Ayuda")
+      })
+  } else if (Mensaje == "estado" || Mensaje == "e") {
+    bot.sendMessage(chatId, "Foco 1 - " + Estado[1])
+      .then(() => {
+        return bot.sendMessage(chatId, "Foco 2 - " + Estado[2])
+      })
+      .then(() => {
+        return bot.sendMessage(chatId, "Foco 3 - " + Estado[3])
+      })
+  } else if (Mensaje == "apagartodo" || Mensaje == "at") {
     client.publish('ALSW/foco1', '0');
     client.publish('ALSW/foco2', '0');
     client.publish('ALSW/foco3', '0');
     bot.sendMessage(chatId, 'Apagar Todo');
-  } else if (MensajeMinus == "encendertodo" || MensajeMinus == "et") {
+  } else if (Mensaje == "encendertodo" || Mensaje == "et") {
     client.publish('ALSW/foco1', '1');
     client.publish('ALSW/foco2', '1');
     client.publish('ALSW/foco3', '1');
     bot.sendMessage(chatId, 'Encender Todo');
-  } else if (MensajeMinus.indexOf("encender") == 0) {
-    var NumeroEncender = MensajeMinus.split("encender");
+  } else if (Mensaje.indexOf("e") == 0) {
+    var NumeroEncender = Mensaje.split(/encender|e| /);
+    console.log("Mensaje " + NumeroEncender[1]);
     CambiarEstado(chatId, NumeroEncender[1], '1');
-  } else if (MensajeMinus.indexOf("apagar") == 0) {
-    var NumeroApagar = MensajeMinus.split("apagar");
-    CambiarEstado(chatId, NumeroApagar[1], '0');
-  } else if (MensajeMinus.indexOf("e") == 0) {
-    var NumeroEncender = MensajeMinus.split("e");
-    CambiarEstado(chatId, NumeroEncender[1], '1');
-  } else if (MensajeMinus.indexOf("a") == 0) {
-    var NumeroApagar = MensajeMinus.split("a");
+  } else if (Mensaje.indexOf("a") == 0) {
+    var NumeroApagar = Mensaje.split(/apagar|a| /);
     CambiarEstado(chatId, NumeroApagar[1], '0');
   } else {
-    bot.sendMessage(chatId, 'Comandos Disponibles, Encender1 o Apagar1 (1-4)');
+    bot.sendMessage(chatId, 'Comandos Disponibles, para mas info \"Ayuda\"');
   }
 });
 
