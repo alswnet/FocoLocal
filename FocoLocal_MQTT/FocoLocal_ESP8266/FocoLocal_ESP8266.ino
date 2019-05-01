bff16976683c40e7dddb3b246352b72788112f61
chepecarlos <chepecarlos@alswblog.org>
2019-04-30 23:02:31 -0600

    Codigo de librerias nueva mqtt

--- FocoLocal_MQTT/FocoLocal_ESP8266/FocoLocal_ESP8266.ino
+++ FocoLocal_MQTT/FocoLocal_ESP8266/FocoLocal_ESP8266.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h>
 #endif
 
-#include <PubSubClient.h>
+#include <MQTT.h>
 
 #define NoWifi 0
 #define Wifi 1
@@ -23,33 +23,53 @@ boolean ConectarMqtt = false;
 boolean ConectarWifi = false;
 int EstadoWifi = NoWifi;
 
-const char* ssid =  "ALSW";
-const char* password = "2526-4897";
-const char* TopicoFoco[4] = {"ALSW/foco/mensaje/1", "ALSW/foco/mensaje/2", "ALSW/foco/mensaje/3"};
-const char* TopicoFocoEstado[4] = {"ALSW/foco/estado/1", "ALSW/foco/estado/2", "ALSW/foco/estado/3"};
+const char* ssid =  "ALSW2";
+const char* password = "7210-3607";
+//const char* ssid =  "ALSW";
+//const char* password = "2526-4897";
+const String TopicoFocoMensaje[4] = {"/ALSW/foco/mensaje/1", "/ALSW/foco/mensaje/2", "/ALSW/foco/mensaje/3"};
+const String TopicoFocoEstado[4] = {"/ALSW/foco/estado/1", "/ALSW/foco/estado/2", "/ALSW/foco/estado/3"};
+
+const char* BrokerMQTT = "broker.shiftr.io";
+int PuertoMQTT = 80;
+const char* ClienteIDMQTT = "FocoALSW";
+const char* UsuarioMQTT = "chepecarlos";
+const char* ContrasenaMQTT = "secretoespecial";
 
 const char* usuario = "chepecarlos";
 const char* pass = "secretoespecial";
 const char* mqtt_server = "broker.shiftr.io";
 char msg[50];
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+WiFiClient ESP_Cliente;
+
+MQTTClient client;
 
 void setup() {
-  for (int i = 0; i < 3; i++) {
-    pinMode(Foco[i], OUTPUT);
-    pinMode(Boton[i], INPUT);
-  }
+  //for (int i = 0; i < 3; i++) {
+  //    pinMode(Foco[i], OUTPUT);
+  //    pinMode(Boton[i], INPUT);
+  // }
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Iniciando :) ");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println("Condigurar MQTT");
+  client.begin("broker.shiftr.io", ESP_Cliente);
+  client.onMessage(PreguntarMqtt);
+
+  ActualizarWifi();
 }
 
 void loop() {
   ActualizarFoco();
   ActualizarBotones();
   ActualizarWifi();
+  ActualizarMQTT();
+  EnviarMQTT();
   ActualizarLed();
 }
 
@@ -76,45 +96,8 @@ void ActualizarLed() {
   }
 }
 
-void ActualizarWifi() {
-  if (!ConectarWifi) {
-    ConectarWifi = true;
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-  }
-
-  if (WiFi.status() != WL_CONNECTED) {
-    EstadoWifi = NoWifi;
-    Serial.println("No Wifi");
-    return;
-  }
-  else {
-    EstadoWifi = Wifi;
-  }
-
-  if (!ConectarMqtt) {
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(PreguntarMqtt);
-    ConectarMqtt = true;
-  }
-
-  if (EstadoWifi >= Wifi && !client.loop() && TiempoMqttReconectar + 500 <  millis()) {
-    TiempoMqttReconectar =  millis();
-    Serial.print("Activando mqtt");
-    if (client.connect("ALSWFOCO")) {//Cambiar por un nombre unico para no tener poblemas
-      Serial.println("... Conectado");
-      for (int i = 0; i < 3; i++) {
-        client.subscribe(TopicoFoco[i]);
-      }
-      EstadoWifi = MqttWifi;
-    } else {
-      Serial.print(" error, rc=");
-      Serial.println(client.state());
-      EstadoWifi = Wifi;
-    }
-  }
-
-  if (TiempoMensaje + 3000 < millis() && EstadoWifi == MqttWifi) {
+void EnviarMQTT() {
+  if (TiempoMensaje + 3000 < millis()) {
     TiempoMensaje = millis();
     //snprintf (msg, 75, "%l", EstadoFoco);
     Serial.print("Estado");
@@ -135,6 +118,61 @@ void ActualizarWifi() {
   }
 }
 
+void ActualizarMQTT() {
+  /*if (!ConectarMqtt ) {
+    client.begin("broker.shiftr.io", ESP_Cliente);
+    client.onMessage(PreguntarMqtt);
+    ConectarMqtt = true;
+    }
+  */
+  client.loop();
+  delay(10);
+
+  if (!client.connected()) {
+    if (WiFi.status() == WL_CONNECTED) {
+      if (TiempoMqttReconectar + 500 <  millis()) {
+        TiempoMqttReconectar =  millis();
+        Serial.print("Activando mqtt");
+        if (!client.connect(ClienteIDMQTT, UsuarioMQTT, ContrasenaMQTT)) {//Cambiar por un nombre unico para no tener poblemas
+          Serial.println("... Error");
+          return;
+        }
+        Serial.println("... Conectado");
+        Serial.println("Subcribiendo a:");
+        for (int i = 0; i < 3; i++) {
+          Serial.println(TopicoFocoMensaje[i]);
+          client.subscribe(TopicoFocoMensaje[i]);
+        }
+        EstadoWifi = MqttWifi;
+      }
+    }
+  }
+}
+
+int SinWifi = 0;
+void ActualizarWifi() {
+  //Configurando Wifi
+  /* if (!ConectarWifi) {
+     ConectarWifi = true;
+     WiFi.mode(WIFI_STA);
+     WiFi.begin(ssid, password);
+    }
+  */
+
+  if (WiFi.status() != WL_CONNECTED) {
+    EstadoWifi = NoWifi;
+    Serial.print("No Wifi ");
+    Serial.println(SinWifi++);
+    delay(500);
+    return;
+  }
+  else if (EstadoWifi != Wifi) {
+    EstadoWifi = Wifi;
+    SinWifi = 0;
+  }
+
+}
+
 void ActualizarBotones() {
   for (int i = 0; i < 3; i++) {
     if (digitalRead(Boton[i]) == 0) {
@@ -147,20 +185,12 @@ void ActualizarBotones() {
   }
 }
 
-void PreguntarMqtt(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensaje recibido [");
-  Serial.print(topic);
-  Serial.print("] ");
-
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-
-  Serial.println();
+void PreguntarMqtt(String & topic, String & payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
 
   int FocoMensaje = -1;
   for (int i = 0; i < 3; i++) {
-    if (strcmp(topic, TopicoFoco[i]) == 0) {
+    if (topic.compareTo(TopicoFocoMensaje[i]) == 0) {
       Serial.print("Topico Foco");
       Serial.println(i + 1);
       FocoMensaje = i;
@@ -168,14 +198,16 @@ void PreguntarMqtt(char* topic, byte* payload, unsigned int length) {
   }
 
   if (FocoMensaje > -1 ) {
-    if ((char)payload[0] == '1') {
+    int ValorEnviado = payload.toInt();
+    if (ValorEnviado) {
       EstadoFoco[FocoMensaje] = true;
     } else {
-      EstadoFoco[FocoMensaje]  = false;
+      EstadoFoco[FocoMensaje] = false;
     }
   } else {
     Serial.println("No Topico Conocido");
   }
+
 }
 
 void ActualizarFoco() {
